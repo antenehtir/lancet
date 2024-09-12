a899ec20195f5f61b040ebeeb7f1c2e57bdeacfa
antenehtir <161809149+antenehtir@users.noreply.github.com>
2024-09-12 10:29:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,35 @@
 document.addEventListener('DOMContentLoaded', function() {
     const specialties = {
-        "Cardiology": ["Dr. Seyfemichael Getachew"],
-        "Endocrinology": ["Dr. Rediet Ambachew"],
-        "Urology": ["Dr. Seid Mohammed"],
-        "Dermatology": ["Dr. Selamawit Worku"],
-        "Pediatrics": ["Dr. Selamawit Assefa"],
-        "Nephrology": ["Dr. Seyfemichael Getachew"],
-        "Internal Medicine": ["Dr. Serkalem Nurilign", "Dr. Seyfemichael Getachew"],
-        "Obstetrics & Gynecology": ["Dr. Sisay Teklu"],
-        "Radiology": ["Dr. Assefa Getachew"],
-        "Neurosurgery": ["Dr. Kibruyisfaw Zewdie"],
-        "Maxillofacial Surgery": ["Dr. Shimelis Megersa"]
+        "Psychiatry": ["Dr. Bezawit Sedika"],
+        "Dental Surgery": ["Dr. Lidya Kebede"],
+        "Neurology": ["Dr. Yared Zenebe"],
+        "Surgery": ["Dr. Woubdel Kiflu"],
+        "Urology": ["Dr. Seid Mohamed"]
     };
-
 
     // Doctor information with image links from Imgur
     const doctorInfo = {
-        "Dr. Seyfemichael Getachew": {
-            schedule: "Monday, Tuesday, Thursday, Saturday: 9:00 LT, Fridays: 5:00 LT",
-            profileImage: "https://i.imgur.com/xvV0Nvn.jpg"
+        "Dr. Bezawit Sedika": {
+            schedule: "Monday - Friday: 9:00 AM - 4:00 PM",
+            profileImage: "https://imgur.com/a/SO81kJ4"
         },
-        "Dr. Rediet Ambachew": {
-            schedule: "Tuesday: 6:00 LT, Monday, Wednesday, Friday: 8:00 LT, Saturday: 3:00 LT",
-            profileImage: "https://i.imgur.com/6ZWbu4n.jpg"
+        "Dr. Lidya Kebede": {
+            schedule: "Tuesday, Thursday: 10:00 AM - 2:00 PM",
+            profileImage: "https://imgur.com/a/SHZL5eZ"
         },
-        "Dr. Seid Mohammed": {
-            schedule: "Monday: 5:00 LT, Tuesday, Thursday: 10:00 LT, Friday: 9:00 LT",
-            profileImage: "https://i.imgur.com/VXahQRI.jpg"
+        "Dr. Yared Zenebe": {
+            schedule: "Monday, Wednesday, Friday: 11:00 AM - 5:00 PM",
+            profileImage: "https://imgur.com/a/kZJKkTO"
         },
-        "Dr. Selamawit Worku": {
-            schedule: "Tuesday, Thursday: 2:00 PM, Wednesday, Saturday: 10:00 AM",
-            profileImage: "https://i.imgur.com/z5rcaeV.jpg"
+        "Dr. Woubdel Kiflu": {
+            schedule: "Wednesday, Saturday: 9:00 AM - 2:00 PM",
+            profileImage: "https://imgur.com/a/v6r4lJy"
         },
-        "Dr. Selamawit Assefa": {
-            schedule: "Monday-Saturday during working hours",
-            profileImage: "https://i.imgur.com/gO02t7J.jpg"
-        },
-        // Add more doctor profiles here
+        "Dr. Seid Mohamed": {
+            schedule: "Tuesday, Thursday, Saturday: 8:00 AM - 12:00 PM",
+            profileImage: "https://imgur.com/a/PCXVDsJ"
+        }
+        // Add more doctor profiles if necessary
     };
 
     const specialtySelect = document.getElementById('specialty');
